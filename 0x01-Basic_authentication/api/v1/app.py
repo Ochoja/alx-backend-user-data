ea4f1e2350df6becc31dba7bf5b291e8e3085961
Daniel Ochoja <danielochoja@yahoo.com>
2024-02-23 18:20:20 +0100

    added comments

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -23,6 +23,8 @@ def not_found(error) -> str:
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
+    """ Unauthorized error handler
+    """
     return jsonify({"error": "Unauthorized"}), 401
 
 
