d435c858629381a5aac6338bd9f8b0fd6db07297
Daniel Ochoja <danielochoja@yahoo.com>
2024-02-23 19:20:25 +0100

    updated

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Auth module"""
+from flask import request
+# from models.user import User
+from typing import List, TypeVar
+
+
+class Auth:
+    """Authentication class"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        return None
