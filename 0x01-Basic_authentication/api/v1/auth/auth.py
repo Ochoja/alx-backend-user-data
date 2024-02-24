55b53e8b1cfae0a4386282ed5297c2109ed3bd9c
Daniel Ochoja <danielochoja@yahoo.com>
2024-02-24 05:14:43 +0100

    updated

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Auth module"""
 from flask import request
-from typing import List
+from typing import List, TypeVar
 
 
 class Auth:
@@ -9,12 +9,12 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Returns path"""
-        return path
+        return False
 
     def authorization_header(self, request=None) -> str:
         """Returns request"""
-        return request
+        return None
 
-    def current_user(self, request=None):
+    def current_user(self, request=None) -> TypeVar('User'):
         """Returns request"""
-        return request
+        return None
