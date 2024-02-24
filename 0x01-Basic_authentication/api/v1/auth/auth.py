378b8e60493b94281b5f11b366d52ccb0fcde847
Daniel Ochoja <danielochoja@yahoo.com>
2024-02-24 06:33:32 +0100

    formatted with linter

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,13 +11,15 @@ class Auth:
         """Requires routes that don'nt need auth |
             Return true if path is require else false
         """
-        if excluded_paths is not None and path:  # Remove trailing slash in excluded paths
+        # Remove trailing slash in excluded paths
+        if excluded_paths is not None and path:
             for i in range(len(excluded_paths)):
                 if excluded_paths[i] != '/' and excluded_paths[i][-1] == '/':
                     excluded_paths[i] = excluded_paths[i][:-1]
 
         # Remove trailing slash in path
-        if path is not None and path != '/' and path[-1] == '/' and excluded_paths:
+        if (path is not None and path != '/'
+                and path[-1] == '/' and excluded_paths):
             path = path[:-1]
 
         if not path or not excluded_paths:  # empty arguments or None
@@ -31,6 +33,6 @@ class Auth:
         """Returns request"""
         return None
 
-    def current_user(self, request=None):
+    def current_user(self, request=None) -> TypeVar('User'):
         """Returns request"""
         return None
