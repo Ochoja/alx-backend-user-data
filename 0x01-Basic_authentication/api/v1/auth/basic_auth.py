2a5054f472a91ffc537a274d1632d25b95185e47
Daniel Ochoja <danielochoja@yahoo.com>
2024-02-27 19:49:44 +0100

    made code pycodestyle compliant

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,8 +7,8 @@ class BasicAuth(Auth):
     """ Basic Authentication
     """
 
-    def extract_base64_authorization_header(self,
-                                            authorization_header: str) -> str:
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
         """Return Base64 part of Authorization header"""
         if type(authorization_header) is not str:
             return None
